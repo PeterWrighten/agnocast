60b6076a61830e538bd3e0e755569ec9b00d6c7f
PeterWrighten <peterwrighten@gmail.com>
2026-03-10 17:03:52 +0900

    feat(agnocastlib, test): add --disable-stdout-logs support and corresponding test
    Signed-off-by: PeterWrighten <peterwrighten@gmail.com>

--- src/agnocastlib/src/node/agnocast_context.cpp
+++ src/agnocastlib/src/node/agnocast_context.cpp
@@ -1,10 +1,18 @@
 #include "agnocast/node/agnocast_context.hpp"
+#include <rcl/arguments.h>
+#include <rcutils/logging.h>
 
 namespace agnocast
 {
 
 Context g_context;
 std::mutex g_context_mtx;
+
+static void noop_log_output_handler(
+  const rcutils_log_location_t *, int, const char *, rcutils_time_point_value_t, const char *,
+  va_list *)
+{
+}
 
 void Context::init(int argc, char const * const * argv)
 {
@@ -20,6 +28,23 @@ void Context::init(int argc, char const * const * argv)
   }
 
   parsed_arguments_ = parse_arguments(args);
+
+  // Apply --disable-stdout-logs if present within --ros-args scope.
+  // There is no public rcl API to extract this flag from rcl_arguments_t, so we scan argv directly.
+  // rcl_logging_configure() cannot be used as it would initialize spdlog and attempt to set up a
+  // rosout publisher (which requires rcl_node_t), neither of which exist in agnocast.
+  bool in_ros_args = false;
+  for (const auto & arg : args) {
+    if (arg == "--ros-args") {
+      in_ros_args = true;
+    } else if (arg == "--") {
+      in_ros_args = false;
+    } else if (in_ros_args && arg == "--disable-stdout-logs") {
+      rcutils_logging_set_output_handler(noop_log_output_handler);
+      break;
+    }
+  }
+
   initialized_ = true;
 }
 
